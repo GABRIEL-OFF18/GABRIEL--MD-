eee18030d3735f49a9ef6ff69944a55a53474e84
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2025-07-25 21:04:34 -0500

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 export async function before(m, { conn }) {
 let name = `ELISABET LA MAS 🔥 CHANNEL`
 let imagenes = ["https://h.uguu.se/OwhnBEii.jpg",
-"https://files.catbox.moe/6dewf4.jpg",
+"https://h.uguu.se/KeYYUBnC.jpg",
 "https://files.catbox.moe/6dewf4.jpg",
 "https://files.catbox.moe/6dewf4.jpg",]
 
